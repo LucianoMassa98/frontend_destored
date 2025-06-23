d3ba1c4f7f5ef8eab80f28328b4322397b713548
cotovillaandreas <cotovillaandreas@gmail.com>
2025-06-23 05:37:03 +0000

    feat: create title

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import './App.css'
-
 function App() {
 
   return (
     <>
-      
+      <h1 className='text-3xl font-bold underline text-center'>
+        DESTORED
+      </h1>
     </>
   )
 }
