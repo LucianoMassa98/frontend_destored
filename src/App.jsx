25d01fc5659ae85e66ac569c8bf71321c9a4e0e6
cotovillaandreas <cotovillaandreas@gmail.com>
2025-06-23 08:47:55 +0000

    feat: se genera el menu, se crean las paginas de login y registro

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,50 @@
+import { Link, Outlet } from 'react-router-dom';
+
 function App() {
 
   return (
     <>
-      <h1 className='text-3xl font-bold underline text-center'>
-        DESTORED
-      </h1>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          background: 'black'
+        }}
+      >
+        <img
+          src="/Logo_Destored.svg"
+          style={{
+            maxWidth: 50,
+          }}
+        />
+        <h1
+          className='text-3xl font-bold'
+          style={{
+            color: 'white'
+          }}
+        >
+          DESTO<span style={{ color: 'purple' }} >RED</span>
+        </h1>
+        <nav
+          style={{
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginLeft: 'auto',
+          }}
+        >
+          <ul className='flex text-xl gap-5 mr-5 font-bold text'>
+            <li className='text-[red]'>
+              <Link to="/login">Iniciar Sesión</Link>
+            </li>
+            <li className='text-white'>
+              <Link to="/register">Registrarse</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div style={{ marginTop: 10 }} className="content">
+        <Outlet /> {/* This is where your nested routes will render */}
+      </div>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+
+// Import your page components (you'll create these files)
+import HomePage from './pages/HomePage.jsx';
+import RegisterPage from './pages/RegisterPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />, // App will act as a layout for all routes
+    children: [
+      {
+        index: true, // This makes HomePage the default route for '/'
+        element: <HomePage />,
+      },
+      {
+        path: 'register',
+        element: <RegisterPage />,
+      },
+      {
+        path: 'login',
+        element: <LoginPage />,
+      }
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
